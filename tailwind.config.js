3103312159a730698b14ffdebf76c4db12d447ee
Mugerwa96 <mugerwaobadiah96@gmail.com>
2023-03-24 14:42:45 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        site:"#0fa9e6",
+      },
+      
+    },
+  },
+  plugins: [],
+}
